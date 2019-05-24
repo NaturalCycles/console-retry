98376c2bd377208bb125e4ef9ce359aa00144bb3
Kristofer Borgström <kristofer.borgstrom@naturalcycles.com>
2019-05-24 12:46:21 +0200

    feat: version verification for releases

--- setup.py
+++ setup.py
@@ -1,9 +1,27 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import setuptools
+import os
+import sys
+from console_retry.version import VERSION
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
+
+
+class VerifyVersionCommand(setuptools.command.install):
+    """Custom command to verify that the git tag matches our version"""
+    description = 'verify that the git tag matches our version'
+
+    def run(self):
+        tag = os.getenv('CIRCLE_TAG')
+
+        if tag != VERSION:
+            info = "Git tag: {0} does not match the version of this app: {1}".format(
+                tag, VERSION
+            )
+            sys.exit(info)
+
 
 setuptools.setup(
     name="console_retry",
@@ -26,4 +44,7 @@ setuptools.setup(
         "console_scripts": ['console-retry = console_retry.retry:main']
     },
     python_requires='>=3.5',
+    cmdclass={
+        'verify': VerifyVersionCommand,
+    }
 )
